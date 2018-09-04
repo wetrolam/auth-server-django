6f9c72a5d6bca546781e8da3bba563a179bd6d85
U-comp\vladek <wetrolam@gmail.com>
2018-09-04 20:01:26 +0200

    Initiate the project

---
+++ secret_template.py
@@ -0,0 +1,6 @@
+# This is a template for the "secret.py" config file that have to be placed at the same directory.
+# The "secret.py" file is not in the repository for security reason.
+# You have to create your own "secret.py" file.
+
+# required by Django
+SECRET_KEY = 'secret-key'
